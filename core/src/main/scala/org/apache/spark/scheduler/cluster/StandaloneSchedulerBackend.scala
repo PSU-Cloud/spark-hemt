3c05cf2975ba1f00f26b32d51672b025d6bae928
yuquanshan <springshan13@gmail.com>
2018-04-14 14:03:47 -0400

    Update sc.executorToHost upon an executor registering with standalone scheduler backend.

--- core/src/main/scala/org/apache/spark/scheduler/cluster/StandaloneSchedulerBackend.scala
+++ core/src/main/scala/org/apache/spark/scheduler/cluster/StandaloneSchedulerBackend.scala
@@ -155,6 +155,7 @@ private[spark] class StandaloneSchedulerBackend(
   override def executorAdded(fullId: String, workerId: String, hostPort: String, cores: Int,
     memory: Int, tokens: Int) {
     sc.executorTokens.put(fullId, tokens)
+    sc.executorToHost.put(fullId, workerId)
     logInfo("Granted executor ID %s on hostPort %s with %d core(s), %s RAM".format(
       fullId, hostPort, cores, Utils.megabytesToString(memory)))
   }
