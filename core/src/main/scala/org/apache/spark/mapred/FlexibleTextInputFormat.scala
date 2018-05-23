ba95ef54b3f6a764bb4dcbd60d19c0b3b5111315
yuquanshan <springshan13@gmail.com>
2018-05-23 00:49:23 -0400

    Implement function getSplits(.) in FlexibleTextInputFormat.

--- core/src/main/scala/org/apache/spark/mapred/FlexibleTextInputFormat.scala
+++ core/src/main/scala/org/apache/spark/mapred/FlexibleTextInputFormat.scala
@@ -17,8 +17,64 @@
 
 package org.apache.spark.mapred
 
-import org.apache.hadoop.mapred.TextInputFormat
+import org.apache.hadoop.fs.{FileStatus, FileSystem, LocatedFileStatus}
+import org.apache.hadoop.mapred.{FileSplit, InputSplit, JobConf, TextInputFormat}
+import org.apache.hadoop.net.NetworkTopology
 
+/**
+ * Unlike TextInputFormat that is used in original Spark, this flexible InputFormat allows:
+ * 1. a split whose size is larger than block size;
+ * 2. splitting a file according to a certain array of weights - splitWeights.
+ */
 class FlexibleTextInputFormat extends TextInputFormat {
+  private var splitWeights = Array[Double]()
 
+  def updateSplitWeights(weights: Array[Double]): Unit = {
+    splitWeights = weights
+  }
+
+  override def getSplits(job: JobConf, numSplits: Int): Array[InputSplit] = {
+    if (splitWeights.length == 0) {
+      super.getSplits(job, numSplits)
+    } else {
+      val proportions = splitWeights.map(_ / splitWeights.sum)
+      val files = listStatus(job)
+      assert(files.length <= 1, "Only support single file!")
+      val clusterMap = new NetworkTopology()
+      // var totalSize = files.map(_.getLen).sum
+      var splits = Array[FileSplit]()
+      for (file <- files) {
+        val path = file.getPath
+        val length = file.getLen
+        if (length != 0) {
+          val fs = path.getFileSystem(job)
+          val blkLocations = if (file.isInstanceOf[LocatedFileStatus]) {
+            file.asInstanceOf[LocatedFileStatus].getBlockLocations
+          } else {
+            fs.getFileBlockLocations(file, 0, length)
+          }
+          if (isSplitable(fs, path)) {
+            var accLen: Long = 0
+            for (i <- 0 until proportions.length - 1) {
+              val splitSize = (length * proportions(i)).toLong
+              // TODO(yuquanshan): currently we don't consider the hosts with this file cached
+              // need to consider them in the future,
+              // see FileInputFormat.getSplitHostsAndCachedHosts
+              val splitHosts = getSplitHosts(blkLocations, accLen, splitSize, clusterMap)
+              splits = splits :+ makeSplit(path, accLen, splitSize, splitHosts)
+              accLen += splitSize
+            }
+            val splitHosts = getSplitHosts(blkLocations, accLen, length - accLen, clusterMap)
+            splits = splits :+ makeSplit(path, accLen, length - accLen, splitHosts)
+          } else {
+            val splitHosts = getSplitHosts(blkLocations, 0, length, clusterMap)
+            splits = splits :+ makeSplit(path, 0, length, splitHosts)
+          }
+        } else {
+          splits = splits :+ makeSplit(path, 0, length, Array[String]())
+        }
+      }
+      splits.map(_.asInstanceOf[InputSplit])
+    }
+  }
 }
